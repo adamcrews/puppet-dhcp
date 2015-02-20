1bb0ed1c8282c4cc32c5b9d3c509e587f6ed3e3b
Adam Crews <adam@pathdefender.com>
2015-02-19 16:07:53 -0800

    Added hostname fix

--- manifests/host.pp
+++ manifests/host.pp
@@ -1,12 +1,16 @@
 define dhcp::host (
   $ip,
   $mac,
-  $comment=undef,
+  $domain_name = undef,
+  $comment     = undef,
 ) {
 
-  $host = $name
-
-  concat_fragment { "dhcp.hosts+10_${name}.hosts":
+  # template uses: 
+  #   $ip to set the ipaddress,
+  #   $mac to set the hwaddress,
+  #   $domain_name to the domain if specified
+  #   $title to set the hostname
+  concat_fragment { "dhcp.hosts+10_${title}.hosts":
     content => template('dhcp/dhcpd.host.erb'),
   }
 }
